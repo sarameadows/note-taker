ee1ca905458a04ba866c76f1797bf6ac0c0667d3
Sara <sameads9@gmail.com>
2022-07-25 12:39:26 -0500

    create app.get routes

--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
 
+const {db} = require('./db/db.json');
+
+//parse incoming string or array ata
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON ata
+app.use(express.json());
+//make javascript and css files easily accessible
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 //HTML route: GET /notes should return notes.html
-//HTML route: GET * should return the index.html file
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
 //API route: GET /api/notes should read db.json and return saved notes as json
-//API route: POST /api/notes should recieve a new note and save it to db.json with unique id, then return to client
+app.get('/api/notes', (req,res) => {
+    res.json(db);
+});
+
+// API route: POST /api/notes should recieve a new note and save it to db.json with unique id, then return to client
+
+//HTML route: GET * should return the index.html file
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
